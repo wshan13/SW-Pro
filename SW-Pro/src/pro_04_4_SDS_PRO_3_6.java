d3af48a72ecb97272fa3372384bbeb8885664c08
wshan13 <woosun.han@samsung.com>
2018-07-06 18:34:19 +0900

    test

--- SW-Pro/src/pro_04_4_SDS_PRO_3_6.java
+++ SW-Pro/src/pro_04_4_SDS_PRO_3_6.java
@@ -1,53 +1,97 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Queue;
+import java.util.Stack;
 
 public class pro_04_4_SDS_PRO_3_6 {
 
+	static int N; 
 	public static void main(String[] args) throws Exception {
 
 		System.setIn(new FileInputStream("pro_04_4_SDS_PRO_3_6.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());		// 배열의 크기 
+		N = Integer.parseInt(br.readLine());		// 배열의 크기 
 
 		int[] arr = new int[N];
 		for(int i = 0 ; i < N ; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
+		
+		//printArray(arr);
+		//System.out.println("");
+		
+		int mid = arr[0];
+		System.out.println(0 + " : " + mid);
+		Stack<Integer> lower = new Stack<Integer>();
+		Stack<Integer> upper = new Stack<Integer>();
+		
+		System.out.println(upper);
+		System.out.println(mid);
+		System.out.println(lower);
+		System.out.println("");
 
-		System.out.println(arr[0]);
-		
-		int[] tmp = Arrays.copyOf(arr, 3);
-		Arrays.sort(tmp);
-		System.out.println(tmp[1]);
-		
-		for(int i = 3 ; i < N ; i = i+2) {
-			//System.out.println(i + ", " + arr[i] + ", "+arr[i+1]+"삽입");
-			//printArray(tmp);
-			tmp = insert(arr[i], arr[i+1], tmp);
-			System.out.println(tmp[1]);
+		for(int i = 1 ; i < N ; i++) {
+			if(arr[i] < mid) {
+				if(lower.empty()) {
+					lower.add(arr[i]);
+				} else {
+					if(arr[i] > lower.peek()) {
+						lower.add(arr[i]);	
+					}
+				}
+			}
+			
+			if(arr[i] > mid) {
+				if(upper.empty()) {
+					upper.add(arr[i]);
+				} else {
+					if(arr[i] < upper.peek()) {
+						upper.add(arr[i]);	
+					}
+				}
+			}
+			
+			if(i > 1 && i%2 == 0) {
+				if(lower.size() < upper.size()) {
+					System.out.println(upper);
+					System.out.println(mid);
+					System.out.println(lower);
+					System.out.println("upper가 큼. 데이터 확인");
+					if(upper.peek() > mid) {
+						lower.push(mid);
+						mid = upper.pop();
+					}
+				}
+				if(lower.size() > upper.size()) {
+					System.out.println(upper);
+					System.out.println(mid);
+					System.out.println(lower);
+					System.out.println("lower가 큼. 데이터 확인");
+					if(lower.peek() < mid) {
+						upper.push(mid);
+						mid = lower.pop();
+					}
+				}
+			}
+			
+
+			System.out.println(upper);
+			System.out.println(mid);
+			System.out.println(lower);
+			System.out.println("");
+
+			if(i%2 == 0) {
+				System.out.println(i + " : " + mid);
+			}
 		}
-	}
-	
-	static int[] insert(int x, int y, int[] source) {
-		
-		int[] tmp = new int[source.length+2];
-		
-		for(int i = 0 ; i < 3 ; i++) {
-			tmp[i] = source[i];
-		}
-		
-		tmp[3] = x;
-		tmp[4] = y;
-		
-		Arrays.sort(tmp);
-		
-		return new int[] {tmp[1], tmp[2], tmp[3]};
+
 	}
 	
 	static void printArray(int[] tmp) {
-		System.out.println("[" + tmp[0] + ", " + tmp[1] + ", " + tmp[2] + "]");
+		for(int i = 0 ; i < N ; i++) {
+			System.out.println(tmp[i]);
+		}
 	}
 }
